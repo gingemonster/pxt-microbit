6cb7f3b40088a49c7d899739f8eee07a05aa3b66
darzu <daryl.zuniga@gmail.com>
2016-07-01 14:47:22 -0700

    merges libmbits, simsvg, and state into simlib

--- sim/simlib.ts
+++ sim/simlib.ts
@@ -199,3 +199,63 @@ namespace pxsim.bluetooth {
         // TODO
     }
 }
+
+namespace pxsim.boardsvg {
+    export interface IPointerEvents {
+        up: string,
+        down: string,
+        move: string,
+        leave: string
+    }
+
+    export const pointerEvents: IPointerEvents = !!(window as any).PointerEvent ? {
+        up: "pointerup",
+        down: "pointerdown",
+        move: "pointermove",
+        leave: "pointerleave"
+    } : {
+        up: "mouseup",
+        down: "mousedown",
+        move: "mousemove",
+        leave: "mouseleave"
+    };
+}
+
+namespace pxsim {
+    export interface RuntimeOptions {
+        theme: string;
+    }
+
+    export class EventBus {
+        private queues: Map<EventQueue<number>> = {};
+
+        constructor(private runtime: Runtime) { }
+
+        listen(id: number, evid: number, handler: RefAction) {
+            let k = id + ":" + evid;
+            let queue = this.queues[k];
+            if (!queue) queue = this.queues[k] = new EventQueue<number>(this.runtime);
+            queue.handler = handler;
+        }
+
+        queue(id: number, evid: number, value: number = 0) {
+            let k = id + ":" + evid;
+            let queue = this.queues[k];
+            if (queue) queue.push(value);
+        }
+    }
+
+    export class DalBoard extends BaseBoard {
+        id: string;
+
+        // the bus
+        bus: EventBus;
+
+        constructor() {
+            super()
+            this.id = "b" + Math_.random(2147483647);
+            this.bus = new EventBus(runtime);
+        }
+        
+    }
+}
